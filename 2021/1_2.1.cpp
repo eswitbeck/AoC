11dd9b234a839d8f1bc27003d5c883c0c6930d8d
eswitbeck <84057541+eswitbeck@users.noreply.github.com>
2021-12-03 11:09:05 -0800

    Add files via upload
    Newer versions in a very ungit fashion to reflect The Great Learning.

---
+++ 2021/1_2.1.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <fstream>
+#include <list>
+using namespace std;
+
+list<int> fileToList (string fileName){
+  ifstream fileStream (fileName);
+  list<int> listOut;
+  string temp;
+  while(getline(fileStream, temp)){
+    listOut.push_back(stoi(temp,nullptr));
+  }
+  return listOut;
+}
+int countIncreases (list<int> listIn){
+  int numIncrease = 0;
+  for (list<int>::iterator it = next(listIn.begin(),3);it != listIn.end(); ++it){
+    if ( *it > *prev(it,3) ){++numIncrease;};
+  }
+  return numIncrease;
+}
+
+int main(){
+  string fileName;
+  list<int> file;
+  cout << "Input filename: ";
+  getline(cin, fileName);
+  file = fileToList(fileName);
+  cout << countIncreases(file);
+  return 0;
+}
