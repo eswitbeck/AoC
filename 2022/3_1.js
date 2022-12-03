13c7fc9620ae27072b95dc3368e4d2573e88938d
eswitbeck <84057541+eswitbeck@users.noreply.github.com>
2022-12-02 22:55:21 -0800

    Add files via upload

---
+++ 2022/3_1.js
@@ -0,0 +1,32 @@
+const {readFileSync} = require('fs');
+
+const listOfSupplies = readFileSync('.\\3_input','utf-8').trimEnd();
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function splitIntoCompartments(str) {
+  let first = str.substring(0, str.length/2);
+  let second = str.substring(str.length/2);
+  return [first, second];
+}
+
+function commonItem([first, second]) {
+  for (let letter of first) {
+    if (second.includes(letter)) return letter;
+  }
+}
+
+let total = listOfSupplies.split('\n').map(
+  str => splitIntoCompartments(str)
+).map(
+  pair => commonItem(pair)
+).map(
+  letter => {
+    if(letter == letter.toUpperCase()) {
+      return alphabet.indexOf(letter.toLowerCase()) + 27;
+    } else {
+      return alphabet.indexOf(letter.toLowerCase()) + 1;
+    }
+  }
+).reduce((a,b) => a + b);
+
+console.log(total);
---
+++ 2022/3_2.js
@@ -0,0 +1,36 @@
+const {readFileSync} = require('fs');
+
+const listOfSupplies = readFileSync('.\\3_input','utf-8').trimEnd();
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function splitIntoTrios (list) {
+  let chunkedArray = [];
+  for (let i = 0; i < list.length; i += 3) {
+    chunkedArray.push(list.slice(i, i + 3));
+  }
+  return chunkedArray;
+}
+
+function commonItem([first, second, third]) {
+  for (let letter of first) {
+    if (second.includes(letter) && third.includes(letter)) return letter;
+  }
+}
+
+function letterValue (letter) {
+  if(letter == letter.toUpperCase()) {
+    return alphabet.indexOf(letter.toLowerCase()) + 27;
+  } else {
+    return alphabet.indexOf(letter.toLowerCase()) + 1;
+  }
+}
+
+let total = splitIntoTrios(
+  listOfSupplies.split('\n')
+).map(
+  trio => commonItem(trio)
+).map(
+  letter => letterValue(letter)
+).reduce((a,b) => a + b);
+
+console.log(total);
