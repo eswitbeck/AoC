a40d94afaa4850e4052e4c755b94cae1e5260070
eswitbeck <84057541+eswitbeck@users.noreply.github.com>
2022-12-04 23:25:46 -0800

    Update 5_1.js

--- 2022/5_1.js
+++ 2022/5_1.js
@@ -5,38 +5,31 @@ input = input.split('\n');
 let start = input.slice(0,8);
 let act = input.slice(10);
 
-let match = /move (\d{1,2}) from (\d) to (\d)/g
-let startMatch = /(   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\]) (   |\[.{1}\])/g
-
 const startObj = [];
 for (let i = 0; i < 9; i++) {
   startObj.push([]);
 }
 
-start.map( e => e.matchAll(startMatch) )
-  .map(match => [...match])
+start.map( e => e.match(/(    |\[.\] )/g))
   .map(e => assignToStart(e, startObj));
 
-act.map( e => e.matchAll(match) )
-  .map(match => [...match])
+act.map( e => e.match(/ \d{1,2}/g ))
   .map(e => pullNumber(e).map(Number))
   .map(e => enactMove(e, startObj))
 
 console.log(
   startObj.map(stack => stack[stack.length - 1])
-  .reduce ((a,b) => a += b, '')
+  .join('')
 )
 
 
-function pullNumber ([matched]) {
-  let out = [...matched];
-  return [out[1], out[2], out[3]];
+function pullNumber (out) {
+  return [out[0], out[1], out[2]];
 }
 
-function assignToStart ([matched], obj) {
-  let out = [...matched].slice(1,10)
+function assignToStart (out, obj) {
   for (let i = 0; i < out.length; i++) {
-    if (out[i] !== '   ') {
+    if (out[i] !== '    ') {
       obj[i].unshift(out[i][1]);
     }
   }
