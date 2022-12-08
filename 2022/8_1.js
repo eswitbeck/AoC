563e3f852d76a6e1a64b27831a0d2ef504caa023
eswitbeck <84057541+eswitbeck@users.noreply.github.com>
2022-12-08 00:23:31 -0800

    Add files via upload

---
+++ 2022/8_1.js
@@ -0,0 +1,79 @@
+class Tree {
+  constructor(height) {
+    this.height = height;
+    this.counted = false;
+  }
+
+  count () {
+    this.counted = true;
+  }
+}
+
+const {readFileSync} = require('fs');
+let input = readFileSync('.\\8_input','utf-8').trimEnd();
+
+let woods = input.split('\n');
+let forest = [], rowMax = [], colMax = [];
+let vis = 0;
+
+ //fill forest 
+for (let i = 0; i < woods.length; i++) {
+  let line = woods[i];
+  forest[i] = [];
+  rowMax[i] = -1;
+  for (let j = 0; j < line.length; j++) {
+    forest[i][j] = new Tree(Number(line[j]));
+    colMax[j] = -1;
+  }
+}
+
+for (let i = 0; i < forest.length; i++) { // L and Top
+  let line = forest[i];
+  for (let j = 0; j < line.length; j++) {
+    if (line[j].height > rowMax[i]) {
+      rowMax[i] = line[j].height;
+      if (!line[j].counted) {
+        vis++;
+        line[j].count();
+      }
+    }
+    if (line[j].height > colMax[j]) {
+      colMax[j] = line[j].height;
+      if (!line[j].counted) {
+        vis++;
+        line[j].count();
+      }
+    }
+  }
+}
+
+//reset maxs
+for (let i = forest.length - 1; i > -1; i--) { //R and Bottom
+  let line = woods[i];
+  rowMax[i] = -1;
+  for (let j = line.length - 1; j > -1; j--) {
+    colMax[j] = -1;
+  }
+}
+
+for (let i = forest.length - 1; i > -1; i--) { //R and Bottom
+  let line = forest[i];
+  for (let j = line.length - 1; j > - 1; j--) {
+    if (line[j].height > rowMax[i]) {
+      rowMax[i] = line[j].height;
+      if (!line[j].counted) {
+        vis++;
+        line[j].count();
+      }
+    }
+    if (line[j].height > colMax[j]) {
+      colMax[j] = line[j].height;
+      if (!line[j].counted) {
+        vis++;
+        line[j].count();
+      }
+    }
+  }
+}
+
+console.log(vis);
---
+++ 2022/8_2.js
@@ -0,0 +1,69 @@
+class Tree {
+  constructor(height, i, j) {
+    this.height = height;
+    this.i = i;
+    this.j = j
+  }
+  
+  scenicScore () {
+    let up = 0, down = 0, left = 0, right = 0;
+    for (let y = this.i - 1; y > -1; y--) {
+      let compare = forest[y][this.j];
+      if (compare.height >= this.height) {
+        up++
+        break;
+      }
+      up++
+    }
+    for (let y = this.i + 1; y < forest.length; y++) {
+      let compare = forest[y][this.j];
+      if(compare.height >= this.height) {
+        down++;
+        break;
+      }
+      down++
+    }
+    for (let x = this.j - 1; x > -1; x--) {
+      let compare = forest[this.i][x];
+      if(compare.height >= this.height) {
+        left++;
+        break;
+      }
+      left++;
+    }
+    for (let x = this.j + 1; x < forest[0].length ; x++) { 
+      let compare = forest[this.i][x];
+      if(compare.height >= this.height) {
+        right++;
+        break;
+      }
+      right++;
+    }
+    return up * down * left * right;
+  }
+}
+
+const {readFileSync} = require('fs');
+let input = readFileSync('.\\8_input','utf-8').trimEnd();
+let woods = input.split('\n');
+let forest = [];
+
+ //fill forest 
+for (let i = 0; i < woods.length; i++) {
+  let line = woods[i];
+  forest[i] = [];
+  for (let j = 0; j < line.length; j++) {
+    forest[i][j] = new Tree(Number(line[j]), i, j);
+  }
+}
+
+let flatList = [];
+
+for (let i = 0; i < forest.length; i++) {
+  let line = forest[i];
+  for (let j = 0; j < line.length; j++) {
+    flatList.push(forest[i][j].scenicScore());
+  }
+}
+
+console.log(flatList.reduce((a,b) => Math.max(a,b)));
