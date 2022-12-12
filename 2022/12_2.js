3c53ae27d42f381bd1e478c2e0af6d9c3c56a656
eswitbeck <84057541+eswitbeck@users.noreply.github.com>
2022-12-12 13:01:47 -0800

    Add files via upload

---
+++ 2022/12_2.js
@@ -0,0 +1,68 @@
+const {readFileSync} = require('fs');
+const input = readFileSync('.\\12_input', 'utf-8').trimEnd().split('\n');
+
+class Node {
+  constructor (val, i, j) {
+    this.val = val;
+    this.i = i;
+    this.j = j;
+    this.neighbors;
+    this.distance = Infinity;
+  }
+
+  checkNeighbors () {
+    const connected = (node, neighbor) => node.val - neighbor.val <= 1;
+    let nCoords = [[this.i - 1, this.j], [this.i, this.j - 1],
+                   [this.i, this.j +1],  [this.i + 1, this.j]]
+    this.neighbors = nCoords.filter(([x, y]) => graph[x] && graph[x][y] &&
+                                    connected(this, graph[x][y]))
+                        .map(([x,y]) => graph[x][y]);
+  }
+}
+
+let graph = [];
+let unvisited = [];
+let startCheck = [];
+
+//convert graph
+for (let i = 0; i < input.length; i++) {
+  let line = input[i];
+  graph[i] = [];
+  for (let j = 0; j < line.length; j++) {
+    let loc = line[j];
+    graph[i][j] = new Node (loc.charCodeAt() - 96, i, j);
+    if (loc == 'S') {
+      start = graph[i][j];
+      start.val = 1;
+    } else if (loc == 'E') {
+      end = graph[i][j];
+      end.distance = 0;
+      end.val = 26;
+    }
+    unvisited.push(graph[i][j]);
+  }
+}
+
+//populate nodes
+for (const n of graph.flat()) n.checkNeighbors();
+
+//Dijkstra
+function traverse (node) {
+  let unvis = node.neighbors.filter(n => unvisited.includes(n));
+  for (const neigh of unvis) {
+    neigh.distance = Math.min(neigh.distance, node.distance + 1)
+  }
+  unvisited.splice(unvisited.indexOf(node),1)
+  startCheck.push(node);
+  if (startCheck.map(n => n.val).includes(1) ||
+      unvisited
+        .map(n => n.distance)
+        .reduce((a,b) => Math.min(a,b)) == Infinity) {
+    return node.distance;
+  } else {
+    let least = unvisited.sort((a,b) => a.distance - b.distance)[0];
+    return traverse(least);
+  }
+}
+
+console.log(traverse(end));
