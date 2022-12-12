1ef51b2665cb6255b4b040b2a2c67a5020aa318f
eswitbeck <84057541+eswitbeck@users.noreply.github.com>
2022-12-12 13:25:36 -0800

    fixing indentation

--- 2022/12_1.js
+++ 2022/12_1.js
@@ -13,7 +13,7 @@ class Node {
   checkNeighbors () {
     const connected = (node, neighbor) => neighbor.val - node.val <= 1;
     let nCoords = [[this.i - 1, this.j], [this.i, this.j - 1],
-                   [this.i, this.j +1],  [this.i + 1, this.j]]
+                   [this.i, this.j + 1],  [this.i + 1, this.j]]
     this.neighbors = nCoords.filter(([x, y]) => graph[x] && graph[x][y] &&
                                     connected(this, graph[x][y]))
                         .map(([x,y]) => graph[x][y]);
@@ -51,16 +51,16 @@ function traverse (node) {
   for (const neigh of unvis) {
     neigh.distance = Math.min(neigh.distance, node.distance + 1)
   }
-  unvisited.splice(unvisited.indexOf(node),1)
-    if (!unvisited.includes(end) ||
-        unvisited
-          .map(n => n.distance)
-          .reduce((a,b) => Math.min(a,b)) == Infinity) {
-      return end.distance
-    } else {
-      let least = unvisited.sort((a,b) => a.distance - b.distance)[0];
-      return traverse(least);
-    }
+  unvisited.splice(unvisited.indexOf(node), 1)
+  if (!unvisited.includes(end) ||
+      unvisited
+        .map(n => n.distance)
+        .reduce((a,b) => Math.min(a,b)) == Infinity) {
+    return end.distance
+  } else {
+    let least = unvisited.sort((a,b) => a.distance - b.distance)[0];
+    return traverse(least);
+  }
 }
 
 console.log(traverse(start));
