3ca47a82680d5fa9f41b1e26fd4941bab41ee77b
Esther Witbeck <esther.witbeck@gmail.com>
2023-04-14 23:27:20 -0700

    needless refactor and attempt to chang submission config

--- 2022/01_1.clj
+++ 2022/01_1.clj
@@ -1,6 +1,7 @@
-(let [input (slurp "input1")]
+(let [listOfCalorieLists 
+      (map #(map read-string (clojure.string/split % #"\n"))
+                   (clojure.string/split (slurp "input1") #"\n\n"))]
   (println 
     (apply max
            (map #(reduce + %) 
-                (map #(map read-string (clojure.string/split % #"\n"))
-                     (clojure.string/split input #"\n\n"))))))
+                listOfCalorieLists))))
