c7350f16a92355bbdfe228792217ec38dba226f5
Esther Witbeck <estherwitbeck@gmail.com>
2023-04-14 23:13:18 -0700

    refactor

--- 2022/01_1.clj
+++ 2022/01_1.clj
@@ -1,20 +1,6 @@
-(def input (with-open [rdr (clojure.java.io/reader "input1")]
-  (reduce conj '() (line-seq rdr))))
-
-(def inputRemap (map (fn [x] (if (= "" x) x (read-string x))) input))
-
-(defn walk [list] 
-  (loop [car (first list) cdr (rest list) count 0 max 0]
-    (if (not car)
-      (println max)
-        (recur
-          (first cdr)
-          (rest cdr)
-          (if (string? car)
-            0
-            (+ count car ))
-          (if (> max count)
-            max
-            count)))))
-
-(walk inputRemap)
+(let [input (slurp "input1")]
+  (println 
+    (apply max
+           (map #(reduce + %) 
+                (map #(map read-string (clojure.string/split % #"\n"))
+                     (clojure.string/split input #"\n\n"))))))
