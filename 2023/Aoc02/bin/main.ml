ec722c06455d3d3c37bc2296acd5ff4ff4059612
Esther Witbeck <esther.witbeck@gmail.com>
2023-12-02 15:25:11 -0800

    Improve readability

--- 2023/Aoc02/bin/main.ml
+++ 2023/Aoc02/bin/main.ml
@@ -11,13 +11,20 @@ let list = read_lines filename
 
 let parse_line l = 
   let open Re.Pcre in 
-  let set = extract ~rex:(regexp "Game (\\d+): (.*)") l in
-  let split_body = split ~rex:(regexp "; ") set.(2) in
-  let extract_game_data game = 
-    let results = extract ~rex:(regexp "(\\d+) (blue|red|green)") game in
+  let split_line = extract ~rex:(regexp "Game (\\d+): (.*)") l in
+  let game_number = int_of_string split_line.(1) in
+  let split_body = split ~rex:(regexp "; ") split_line.(2) in
+  let extract_pull pull = 
+    let results = extract ~rex:(regexp "(\\d+) (blue|red|green)") pull in
     (int_of_string results.(1), results.(2)) in
-  (int_of_string set.(1), List.map (split ~rex:(regexp ", ")) split_body |> List.map @@ List.map extract_game_data)
+  (
+    game_number,
+    split_body 
+      |> List.map (split ~rex:(regexp ", "))
+      |> List.map @@ List.map extract_pull
+  )
 
+(* syntax is: games are made of rounds are made of pulls *)
 let is_valid_game (_ , body) = 
   let is_valid_pull (count, color) =
     match color with
@@ -27,7 +34,7 @@ let is_valid_game (_ , body) =
     | _ -> raise Not_found in
   let is_valid_round = List.for_all is_valid_pull in
   List.map is_valid_round body
-  |> List.fold_left ( && ) true
+    |> List.for_all (fun a -> a)
 
 let () = list
   |> List.map parse_line
@@ -36,25 +43,22 @@ let () = list
   |> List.fold_left ( + ) 0
   |> Printf.printf "Solution 1: %d\n"
       
-let max_across_pulls (blue, green, red) (count, color) =
-  match color with
-  | "blue" -> (Int.max blue count, green, red)
-  | "green" -> (blue, Int.max green count, red)
-  | "red" -> (blue, green, Int.max red count)
-  | _ -> raise Not_found
-
-let get_round_count = List.fold_left max_across_pulls (0, 0, 0)
-
-let max_across_rounds (b1, g1, r1) (b2, g2, r2) =
-  (Int.max b1 b2, Int.max g1 g2, Int.max r1 r2)
-
-let get_counts (_, body) =
+let get_min_counts (_, body) =
+  let max_across_pulls (blue, green, red) (count, color) =
+    match color with
+    | "blue" -> (Int.max blue count, green, red)
+    | "green" -> (blue, Int.max green count, red)
+    | "red" -> (blue, green, Int.max red count)
+    | _ -> raise Not_found in 
+  let get_round_count = List.fold_left max_across_pulls (0, 0, 0) in 
+  let max_across_rounds (b1, g1, r1) (b2, g2, r2) =
+    (Int.max b1 b2, Int.max g1 g2, Int.max r1 r2) in 
   List.map get_round_count body
-  |> List.fold_left max_across_rounds (0, 0, 0)
+    |> List.fold_left max_across_rounds (0, 0, 0)
 
 let () = list
   |> List.map parse_line
-  |> List.map get_counts
+  |> List.map get_min_counts
   |> List.map (fun (b, g, r) -> b * g * r)
   |> List.fold_left ( + ) 0
   |> Printf.printf "Solution 2: %d\n"
