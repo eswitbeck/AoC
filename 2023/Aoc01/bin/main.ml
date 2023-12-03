ed87a76b5eae78ff237de70453ceb90b3d805f2a
Esther Witbeck <esther.witbeck@gmail.com>
2023-12-02 18:19:46 -0800

    Add functioning day 1 OCaml solution

--- 2023/Aoc01/bin/main.ml
+++ 2023/Aoc01/bin/main.ml
@@ -16,40 +16,28 @@ let process_string s =
   explode_string s
     |> List.filter (pmatch ~rex:(regexp "\\d"))
     |> List.fold_left ( ^ ) ""
-    |> (fun s -> Char.escaped (String.get s 0) ^ Char.escaped (String.get s (String.length s - 1)))
+    |> (fun s -> Char.escaped (String.get s 0) ^ Char.escaped @@ String.get s @@ String.length s - 1)
     |> int_of_string
 
-(*
-let () = 
-  List.iter
-    (fun n -> Printf.printf "%d\n" (process_string n))
-    list
-    *)
-  
 let solution_1 = list
   |> List.map process_string
   |> List.fold_left ( + ) 0
 
-(* write maps for each, replace, run same thing *)
-
-let num_references = ["one", "1"; "two", "2"; "three", "3"; "four", "4"; "five",
-"5"; "six", "6"; "seven", "7"; "eight", "8"; "nine", "9"]
+(* a horrible hack *)
+let num_references = ["one", "o1e"; "two", "t2o"; "three", "t3e"; "four", "f4r"; "five",
+"f5e"; "six", "s6x"; "seven", "s7n"; "eight", "e8t"; "nine", "n9e"]
 
 let new_process_string s =
   let open Re.Pcre in
-    Printf.printf "%s\n" s;
     List.fold_left 
       (fun acc (f, t) -> substitute ~rex:(regexp f) ~subst:(fun _ -> t) acc)
       s
       num_references
-    |> fun l -> Printf.printf "%s\n" l; l
     |> explode_string
     |> List.filter (pmatch ~rex:(regexp "\\d"))
     |> List.fold_left ( ^ ) ""
-    |> (fun s -> Char.escaped (String.get s 0) ^ Char.escaped (String.get s (String.length s - 1)))
-    |> fun l -> Printf.printf "%s\n" l; l
+    |> (fun s -> Char.escaped (String.get s 0) ^ Char.escaped @@ String.get s @@ String.length s - 1)
     |> int_of_string
-    |> fun l -> Printf.printf "%d\n" l; l
 
 
 let solution_2 = list
@@ -57,6 +45,6 @@ let solution_2 = list
   |> List.fold_left ( + ) 0
 
 let () = 
-  Printf.printf "%d\n" solution_1
+  Printf.printf "Solution 1: %d\n" solution_1
 let () = 
-  Printf.printf "%d\n" solution_2
+  Printf.printf "Solution 2: %d\n" solution_2
