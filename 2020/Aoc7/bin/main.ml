97c75eb92746351e2363d1a7bd920755593d7bf6
Esther Witbeck <esther.witbeck@gmail.com>
2023-12-02 22:55:42 -0800

    sketch part 2

---
+++ 2020/Aoc7/bin/main.ml
@@ -0,0 +1,11 @@
+let filename = "input"
+
+let read_file file =
+  In_channel.with_open_bin file In_channel.input_all
+
+let read_lines file =
+  let contents = String.trim (read_file file) in
+  String.split_on_char '\n' contents
+
+let list = read_lines filename
+
