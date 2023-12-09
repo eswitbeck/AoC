a30ccfb2ba44dcb5db7aab6a3ac6bf264ffac050
Esther Witbeck <esther.witbeck@gmail.com>
2023-12-08 19:57:46 -0800

    solve part 2

--- 2023/Aoc08/bin/main.ml
+++ 2023/Aoc08/bin/main.ml
@@ -47,3 +47,39 @@ let rec cascade_directions map key i =
 let () =
   cascade_directions input.map "AAA" 0
   |> Printf.printf "Part 1 solution: %d\n"
+
+
+let keys = String_map.bindings input.map |> List.map fst
+let starting_points = List.filter
+  (fun s -> Re.Pcre.(pmatch ~rex:(regexp "..A") s))
+  keys
+
+let rec cascade_directions_2 map key i = 
+  match key with
+  | k when Re.Pcre.(pmatch ~rex:(regexp "..Z") k) -> i
+  | k -> let p = String_map.find k map in
+    cascade_directions_2 map (follow_directions i p) (i + 1)
+
+let get_factors n =
+  let rec aux m i fs =
+    match m with
+    | 1 -> fs
+    | _ when m mod i = 0 -> aux (m / i) i (i::fs)
+    | _ -> aux m (i + 1) fs
+  in
+  aux n 2 []
+
+let nonduplicated_factors bs cs =
+  let module Int_set = Set.Make(Int) in
+  let c_set = Int_set.of_list cs in
+  let remaining_bs = List.filter (fun i -> not @@ Int_set.mem i c_set) bs in
+  remaining_bs @ cs
+
+let least_common_multiple m n =
+  let multiplicands = nonduplicated_factors (get_factors m) (get_factors n) in
+  List.fold_left ( * ) 1 multiplicands
+
+let () = starting_points
+|> List.map (fun k -> cascade_directions_2 input.map k 0)
+|> List.fold_left least_common_multiple 1
+|> Printf.printf "Part 2 solution: %d\n"
